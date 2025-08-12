edc02c3ea2a2cf22956d3b164b818fb324a26f5e
htmlbasic <withjanak@gmail.com>
2025-08-12 12:45:11 +0545

    भाषा मिलायको

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -91,7 +91,7 @@ export default function Navbar() {
                 to="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
               >
-                गृहपृषठ
+                गृहपृष्ठ
               </Link>
             </li>
             <li>
