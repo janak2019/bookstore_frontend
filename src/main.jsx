2d6bb88461e804cefa3ef7dd2505e695a2a2ccb9
htmlbasic <withjanak@gmail.com>
2025-08-15 17:03:29 +0545

    main.jx chng

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import React, { StrictMode } from "react";
+import {createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  
-);
+  </StrictMode>,
+)
 
